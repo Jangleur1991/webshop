036a9ab79f891fe17b036562cbb8136fa56efd02
Jangleur1991 <Jangleur@web.de>
2023-03-08 20:26:11 +0000

    use ResponseEntity

--- rest-api.md
+++ rest-api.md
@@ -1,6 +1,15 @@
 ## REST Api
 
+### Resources:
+* Products
+* Orders
+* Customer
+
+
+### Endpoints:
 * load all products   
 GET /api/products
 * load product with id  
 GET /api/products/{id}
+* load all products with tag  
+GET /api/products?tag={tag}
--- src/main/java/mygroup/webshop/controller/ProductController.java
+++ src/main/java/mygroup/webshop/controller/ProductController.java
@@ -3,12 +3,14 @@ package mygroup.webshop.controller;
 import mygroup.webshop.model.ProductResponse;
 import mygroup.webshop.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ProductController {
@@ -21,8 +23,10 @@ public class ProductController {
         return repository.findAll(tags);
     }
 
-    @GetMapping("products/{id}")
-    public ProductResponse getProductById(@PathVariable String id) {
-        return repository.findById(id).orElse(null);
+    @GetMapping("/products/{id}")
+    public ResponseEntity<ProductResponse> getProductById(@PathVariable String id) {
+        Optional<ProductResponse> product = repository.findById(id);
+        return product.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
